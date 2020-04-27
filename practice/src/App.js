44fe2bfd478eeb396a94dfcb1736f196fff90571
Joe <63022770+Kluberton@users.noreply.github.com>
2020-04-27 11:57:01 -0800

    Solar Calculator HTML files

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// to push to GitHub in the terminal run this:
+// git add . the period says what you want to add and stages it for commitment to github
+// git status tells you what has or has not been adds
+// git commit -m "message you want to give" the -m is message
+// git push sends things from my computer up to github
+// git pull brings things in from github
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to words
+          Edit <code>src/App.js</code> and save to words to add
         </p>
         <a
           className="App-link"
